15198a0d891831078f9ffb5e8901821582c07233
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-07-04 12:19:45 -0700

    Add simple Rest Controller to the basic application

---
+++ src/main/java/com/mikehashimoto/home/budget/HomeBudgetRestController.java
@@ -0,0 +1,27 @@
+package com.mikehashimoto.home.budget;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeBudgetRestController {
+
+	@PostMapping
+	public ResponseEntity<String> process(@RequestBody String body) {
+		if (_log.isInfoEnabled()) {
+			_log.info("Processing " + body);
+		}
+
+		return new ResponseEntity<>(body, HttpStatus.OK);
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		HomeBudgetRestController.class);
+
+}
